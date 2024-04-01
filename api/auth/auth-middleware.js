90f55aacc7d57f4b87f3ca68155011bdf2910bff
elizabethvilloria <lizvilloria@gmail.com>
2024-04-01 18:51:01 +0200

    Finished mvp

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -70,7 +70,7 @@ async function checkUsernameExists(req, res, next) {
 */
 function checkPasswordLength(req, res, next) {
   if (!req.body.password || req.body.password.length < 3) {
-    next({ message: "Password must be longer than 2 chars", status: 422})
+    next({ message: "Password must be longer than 3 chars", status: 422})
   } else {
     next()
   }
